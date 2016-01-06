ed939a76977d01ff4eabbb507e572242814dadcc
Emmanuel Cuevas <emmanuelcuevas@emmanuels-macbook-pro.local>
2016-01-06 11:46:05 -0600

    Initial Commit

---
+++ ThousandWordsProjectTests/ThousandWordsProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  ThousandWordsProjectTests.m
+//  ThousandWordsProjectTests
+//
+//  Created by Emmanuel Cuevas on 1/6/16.
+//  Copyright © 2016 Emmanuel Cuevas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ThousandWordsProjectTests : XCTestCase
+
+@end
+
+@implementation ThousandWordsProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
